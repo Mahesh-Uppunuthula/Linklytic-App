f8811a2803ae017e2417c4ea46da1f85fa326edf
Mahesh-Uppunuthula <uppunuthulamahesh312@gmail.com>
2024-12-24 02:19:31 +0530

    Added User Menu Component

---
+++ src/Components/Menu/UserMenu.tsx
@@ -0,0 +1,18 @@
+const UserMenu = () => {
+  return (
+    <div
+      title="Mahi312"
+      className="flex gap-2 place-items-center text-gray-400 cursor-pointer"
+    >
+      <div className="w-8 h-8 overflow-hidden rounded-3xl">
+        <img
+          src="../../../assets/images/male-profile-image.webp"
+          alt="male profile picture"
+        />
+      </div>
+      <span>Mahi312</span>
+    </div>
+  );
+};
+
+export default UserMenu;
