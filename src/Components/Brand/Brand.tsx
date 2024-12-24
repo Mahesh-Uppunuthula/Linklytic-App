54a9587488f84317678642792d71de7bcdd4b480
Mahesh-Uppunuthula <uppunuthulamahesh312@gmail.com>
2024-12-25 03:29:33 +0530

    Modified Brand Component for efficient navigation

--- src/Components/Brand/Brand.tsx
+++ src/Components/Brand/Brand.tsx
@@ -1,16 +1,20 @@
 import { FiLink } from "react-icons/fi";
 import Link from "../Button/Link";
 import { PATH_CONSTANTS } from "../../Routes/pathConstants";
+import useAuth from "../../hooks/useAuth";
+import { useLocation } from "react-router-dom";
 type BrandProps = {
   size?: string | number;
   showText?: boolean;
 };
 
 function Brand({ size = "1.5rem", showText = true, ...props }: BrandProps) {
+  const { isAuthenticated } = useAuth();
+  const location = useLocation();
   return (
     <div {...props}>
       <Link
-        to={PATH_CONSTANTS.HOME}
+        to={!isAuthenticated ? PATH_CONSTANTS.HOME : location.pathname}
         iconBefore={
           <FiLink
             id="brand-logo"
